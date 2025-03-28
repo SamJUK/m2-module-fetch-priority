a94f1aa2b723de1bcff41bbcef573cd4938535b7
Sam James <sam@samdjames.uk>
2025-03-28 00:48:24 +0000

    fix: remove test asset

--- ViewModel/Links.php
+++ ViewModel/Links.php
@@ -7,17 +7,8 @@ namespace SamJUK\FetchPriority\ViewModel;
 class Links implements \Magento\Framework\View\Element\Block\ArgumentInterface
 {
     public function __construct(
-        private readonly \SamJUK\FetchPriority\Model\LinkStore $linkStore,
-        private readonly \SamJUK\FetchPriority\Model\Links\PreloadFactory $preloadFactory
-    ) {
-        $preload = $this->preloadFactory->create([
-            'href' => 'https://app.magento2.test/media/my_custom_entity/image1.jpg',
-            'mimeType' => \SamJUK\FetchPriority\Enum\Preload\MimeType::ImageJPEG,
-            'asType' => \SamJUK\FetchPriority\Enum\Preload\AsType::Image,
-            'fetchPriority' => \SamJUK\FetchPriority\Enum\FetchPriority::High
-        ]);
-        $this->linkStore->add($preload);
-    }
+        private readonly \SamJUK\FetchPriority\Model\LinkStore $linkStore
+    ) { }
 
     /**
      * @return \SamJUK\FetchPriority\Api\LinkInterface[]
